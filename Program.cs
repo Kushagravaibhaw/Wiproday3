c56384060fa267e4e25338004477cfc8118e23ab
Kushagravaibhaw <118000465+Kushagravaibhaw@users.noreply.github.com>
2025-01-30 16:28:30 +0530

    Add files via upload
    this is abstractclass

--- Program.cs
+++ Program.cs
@@ -1,42 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-using Wiproday4;
+using SampleAbstractcls;
 
-
-
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        // Employee emp = new Employee();
-        //  emp.Empid = 11;
-        //  emp.EmpName = "Test";
+        //Abstract class cannot be instantiated
+        //It is not mandatory to have abstract method in abstract class.
+        //Generally,we go for abstract in order to show essential features and hide mthd impl
+        // Flight fli = new Flight();
+        //Object Initializer - At the time of object creation,initialize the values
+        FlightFare flightFare = new FlightFare() { FlightNo = 11, FlightName = "AirIndia", durationinhrs = 6, typedes = "International" };
+        flightFare.FlightDetails();
+        flightFare.FareDetails();
 
-        ////  emp.Display();
-
-        //  Department dpt = new Department(); //creating instance for the derived class
-        //  dpt.Empid = 111;
-        //  dpt.EmpName = "Priya";
-        //  dpt.DeptName = "Develoepr";
-        //  dpt.Salary = 50000;
-
-        //dpt.Display();
-        //  dpt.Show();
-
-        //  Manager mgr = new Manager();
-        //mgr.Empid = 111;
-        //mgr.EmpName = "Priya";
-        //mgr.DeptName = "Develoepr";
-        //mgr.Salary = 50000;
-        //mgr.ManagerName = "Riyaz";
-        //mgr.Display();
-        int id = Convert.ToInt32(Console.ReadLine());
-        string name = Console.ReadLine();
-        string dname = Console.ReadLine();
-        decimal sal = Convert.ToDecimal(Console.ReadLine());
-        string mname = Console.ReadLine();
-        Manager mgrcon = new Manager(id, name, dname, sal, mname);
-        mgrcon.Display();
-
+        //create instance for FlightFare by making as a reference to Flight which is abstract class
+        Flight ff = new FlightFare() { FlightNo = 12, FlightName = "Spicejet", durationinhrs = 3, typedes = "National" };
+        ff.FlightDetails();
+        ff.FareDetails();
     }
 }
